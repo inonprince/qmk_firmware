78dd4b6109cd22dfa77a7e23aae9efc608a21373
Inon Prince <inon.prince@echostar.com>
2017-04-29 20:22:01 -0600

    shift-bksps, mod_interrupt

--- keyboards/ergodox/keymaps/inonio/keymap.c
+++ keyboards/ergodox/keymaps/inonio/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_GRV,
         KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,           KC_Y,           TG(1),
-        KC_CAPS,        KC_A,           LT(2, KC_O),    LT(4, KC_E),    LT(3, KC_U),    KC_I,
+        KC_CAPS,        KC_A,           LT(2, KC_O),    KC_E,           LT(3, KC_U),    KC_I,
         KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,           KC_X,           MO(1),
         KC_SLSH,        KC_LBRC,        KC_LCTRL,       KC_LALT,        KC_LGUI,
                                                                                         KC_UP,          KC_DOWN,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(2),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_ESC,
         KC_EQL,         KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           KC_TAB,
                         KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           MO(1),
-        KC_MINS,        KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           KC_RSFT,
+        KC_MINS,        KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           SFT_T(KC_BSPC),
                                         KC_RGUI,        KC_RALT,        KC_RCTRL,       KC_RBRC,        KC_BSLS,
         KC_UP,          KC_DOWN,
         KC_PGUP,
@@ -159,7 +159,7 @@ KEYMAP(
 KEYMAP(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_ACL1, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_ACL1, MO(4),   KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
