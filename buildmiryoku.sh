e41509f19df974cf045b1bfe9fd99638e76596e3
Inon Prince <inon.prince@oracle.com>
2024-04-01 20:38:37 -0700

    build script

---
+++ buildmiryoku.sh
@@ -0,0 +1,6 @@
+qmk compile -c -kb ergodox_ez -km manna-harbour_miryoku \
+-e MIRYOKU_ALPHAS=DVORAK \
+-e MIRYOKU_EXTRA=COLEMAKDH \
+-e MIRYOKU_TAP=DVORAK \
+-e MIRYOKU_CLIPBOARD=MAC \
+-e MIRYOKU_LAYERS=FLIP
